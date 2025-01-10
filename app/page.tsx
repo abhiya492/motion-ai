4a1770baef0dee61df9206a22fd0c369983608a2
abhishek singh <me@example.com>
2025-01-10 17:21:18 +0530

    Dashboard functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
+import BgGradient from "@/components/common/bg-gradient";
 import Banner from "@/components/home/banner";
-import { Dot } from "lucide-react";
 import HowItWorks from "@/components/home/howitworks";
 import Pricing from "@/components/home/pricing";
-import BgGradient from "@/components/common/bg-gradient";
-
+import { Dot } from "lucide-react";
 
 export default function Home() {
   return (
     <main className="mx-auto w-full inset-0 h-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
       <BgGradient />
-
       <Banner />
       <div className="flex items-center justify-center">
         <Dot className="text-purple-400"></Dot>
@@ -33,10 +31,10 @@ export default function Home() {
 
       <footer className="bg-gray-200/20 flex h-20 py-24 px-12 z-20 relative overflow-hidden flex-col gap-2">
         <p>All Rights Reserved, {new Date().getFullYear()}</p>
-        <a href="https://x.com/@kulkarniankita9" target="_blank">
+        <a href="https://x.com/ABHISHE96010387" target="_blank">
           Built by Abhishek 🚀
         </a>
       </footer>
     </main>
-      );
+  );
 }
