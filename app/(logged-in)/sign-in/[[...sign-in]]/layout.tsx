c75dd2d2f0398c48008ea2cec86f2c33333d6920
Abhishek Singh <117256099+abhiya492@users.noreply.github.com>
2025-01-12 10:54:59 +0530

    Update layout.tsx

--- app/(logged-in)/sign-in/[[...sign-in]]/layout.tsx
+++ app/(logged-in)/sign-in/[[...sign-in]]/layout.tsx
@@ -8,7 +8,7 @@ export default function SignInLayout({ children }: { children: ReactNode }) {
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
         <div className="flex flex-col items-center justify-center gap-6 text-center">
           <Badge className="bg-gradient-to-r from-blue-700 to-green-800 text-white px-4 py-1 text-lg font-semibold capitalize">
-            Sign In
+            Hey Man,Welcome to Motion-AI.Please Sign-In to Enjoy Motion-AI
           </Badge>
           {children}
         </div>
